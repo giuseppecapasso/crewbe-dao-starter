65d6b497480eb5e0ac4ef9cee76cc376c40d0d14
giuseppecapasso <capasso.gc@gmail.com>
2022-07-14 16:42:06 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
 const App = () => {
+  // Use the hooks thirdweb give us.
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋 Indirizzo:", address);
+
+  // This is the case where the user hasn't connected their wallet
+  // to your web app. Let them call connectWallet.
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Benvenuto su CrewbeDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connetti il tuo wallet
+        </button>
+      </div>
+    );
+  }
+
+  // This is the case where we have the user's address
+  // which means they've connected their wallet to our site!
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
+      <h1>👀 wallet connected, now what!</h1>
+      <h5>👋 {address}</h5>
+    </div>);
 };
 
 export default App;
